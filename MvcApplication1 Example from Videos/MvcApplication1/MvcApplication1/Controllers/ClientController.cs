335f868c502dead27e03ed126705726e37088875
andreyjs26 <andreyjs260393@gmail.com>
2017-07-04 15:30:53 -0600

    first commit

---
+++ MvcApplication1 Example from Videos/MvcApplication1/MvcApplication1/Controllers/ClientController.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication1.Controllers
+{
+    public class ClientController : Controller
+    {
+        //
+        // GET: /Client/
+
+        public ActionResult ListClients()
+        {
+            var client = new[]
+         {
+                
+         
+               new Models.Client()
+            {
+                Id = Convert.ToInt32("1"),
+                Name = "Andrey",
+                Date = DateTime.Now,
+                Salary = Convert.ToDouble("500")
+             },
+             new Models.Client()
+             {
+             Id = Convert.ToInt32("2"),
+                Name = "Miriam",
+                Date = DateTime.Now,
+                Salary = Convert.ToDouble("1000")
+             
+             }
+
+
+            //ViewData["Client"] = client;
+        };
+            //return View();
+            return View(client);
+        }
+
+        public ActionResult TestTemp()
+        {
+            TempData["message"] = "The action succeeded!";
+            return RedirectToAction("Client");
+
+        }
+
+
+        public ActionResult Client(Int32 id)
+        {
+
+            var client = new MvcApplication1.Models.Client()
+
+            {
+                Id = Convert.ToInt32("1"),
+                Name = "Andrey",
+                Date = DateTime.Now,
+                Salary = Convert.ToDouble("500")
+
+
+
+            };
+
+            ViewData["Client"] = client;
+
+            //return View();
+            return View(client);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            var salary = new SelectList(new[] { "$500", "$1000", "$2000" });
+            ViewBag.Salary = salary;
+            return View();
+
+        }
+        [HttpPost]
+        public ActionResult Create([Bind(Exclude="Salary")]Models.Client client)
+        {
+
+            if (string.IsNullOrWhiteSpace(client.Name))
+            {
+                ModelState.AddModelError("Name", "Name is required");
+            }
+
+
+
+            if (ModelState.IsValid)
+            {
+                //save to db
+                return RedirectToAction("ListClients");
+            }
+
+            return Create();
+        }
+
+
+    }
+}
---
+++ MvcApplication1 Example from Videos/MvcApplication1/MvcApplication1/Models/Client.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcApplication1.Models
+{
+    public class Client
+    {
+        [Required]
+        public int Id {get;set;}
+
+        [Required]
+        [DataType(DataType.Text)]
+        [StringLength(maximumLength:200, MinimumLength=5)]
+        public string Name{get;set;}
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime Date {get;set;}
+
+        [Required]
+        [DataType(DataType.Currency)]
+        public double Salary { get; set; }
+
+
+    }
+}
